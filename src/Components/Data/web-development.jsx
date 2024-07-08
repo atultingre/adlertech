125b1b4161b74bbc9acb93588f1a97f595876a36
Atul Tingre <atultingre.work@gmail.com>
2024-07-08 15:02:23 +0530

    webdevelopment

---
+++ src/Components/Data/web-development.jsx
@@ -0,0 +1,44 @@
+export const webDevelopment = [
+  {
+    image: "./software-developer.png",
+    title: "Custom Website Development",
+    description:
+      "We create custom websites that support your company’s goals. Our bespoke development services guarantee that your website is distinctive and offers a special user experience. We can help with whatever type of website you require, including personal blogs, e-commerce platforms, and corporate websites.",
+  },
+  {
+    image: "./responsive-design.png",
+    title: "Responsive Web Design",
+    description:
+      "A website that is mobile-first is vital in today’s society. Our staff makes sure that your website offers the best possible viewing experience on PCs, tablets, and smartphones. This raises your SEO ranks and enhances user experience.",
+  },
+  {
+    image: "./software.png",
+    title: "Content Management Systems (CMS)",
+    description:
+      "Our expertise lies in creating and modifying robust content management systems such as WordPress, Joomla, and Drupal. Without requiring technical knowledge, our content management systems (CMS) enable you to effortlessly handle the content, updates, and upkeep of your website.",
+  },
+  {
+    image: "./troubleshooting.png",
+    title: "Website Maintenance and Support",
+    description:
+      "For your website to remain safe and function properly, it requires regular upgrades and maintenance. To keep your website safe, secure, and functional, we provide regular support and maintenance services.",
+  },
+  {
+    image: "./search-engine-optimization.png",
+    title: "SEO Optimization",
+    description:
+      "A well-designed website needs to be simple to locate as well. On-page SEO techniques are part of our web construction process, which raises your website’s search engine ranking. To help you rank better and draw in organic visitors, we improve your site’s content, meta tags, and structure, among other things.",
+  },
+  {
+    image: "./performance-opti.png",
+    title: "Performance Optimization",
+    description:
+      "Both SEO and customer pleasure depend on a website’s speed. We optimize your website to run smoothly and load quickly. This entails enhancing server response speeds, utilizing browser caching, and optimizing pictures.",
+  },
+  {
+    image: "./operations.png",
+    title: "Integration Services",
+    description:
+      "We integrate your website with essential tools and third-party services such as CRM systems, payment gateways, social media platforms, and marketing automation tools. These integrations streamline your business processes and enhance functionality.",
+  },
+];
--- src/Components/Tech/WebDevelopment.jsx
+++ src/Components/Tech/WebDevelopment.jsx
@@ -1,10 +1,24 @@
-import React from "react";
 import Layout from "../Dashboard/Layout";
+import { webDevelopment } from "../Data/web-development";
+import Card from "./Card";
 
 const WebDevelopment = () => {
   return (
     <Layout>
-      <div className="p-10">WebDevelopment</div>
+      <div className='pt-10'>
+        <h1 className="text-3xl font-semibold text-start mb-5">Social Media Marketing</h1>
+
+        <div className="py-5 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {webDevelopment?.map((item, index) => (
+            <Card
+              key={index}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 };
